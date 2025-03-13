12b1add934d117f642ac5c96f6da5d5d56c86108
ruchithars <ruchram9@gmail.com>
2025-03-13 12:48:23 +0530

    changes for task4

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hiiiiii";
+  cout << "How are youu";
+  cout << "I have successfully built and run this im ruchitha \n";
   return 0;
 }
